df4aa6b8d3b789c01a6f7865ac2ea7a390c98ae5
lee <q2298478324@gmail.com>
2019-10-14 19:47:51 +0900

    add sophus library

---
+++ Sophus/Sophus/Sophus.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "sophus\so3.h"
+#include "sophus\se3.h"
+
+int main()
+{
+	Eigen::Matrix3d R = Eigen::AngleAxisd(EIGEN_PI / 2, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
+	Sophus::SO3 SO3_R(R);
+	Sophus::SO3 SO3_v(0, 0, EIGEN_PI / 2);
+	Eigen::Quaterniond q(R);
+	Sophus::SO3 SO3_q(q);
+	cout << "SO(3) from matrix: " << SO3_R << endl;
+	cout << "SO(3) from vector: " << SO3_v << endl;
+	cout << "SO(3) from quaternion :" << SO3_q << endl;
+
+}
+
---
+++ Sophus/Sophus/pch.h
@@ -0,0 +1,9 @@
+﻿
+
+#ifndef PCH_H
+#define PCH_H
+
+#include "sophus\so3.h"
+#include "sophus\se3.h"
+
+#endif //PCH_H
